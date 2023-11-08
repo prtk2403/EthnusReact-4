03f2f56242b583ea1a012ab6c0f92dba4693d0cd
prtk2403 <prtk.2403@gmail.com>
2023-11-08 22:08:17 +0530

    Updated Source Files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Details from './components/Info';
 function App() {
   return (
     <div className="App">
+      <h3>Created By Pratheek Nistala <a href='https://github.com/prtk2403'>prtk2403</a></h3>
       <Details/>
     </div>
   );
